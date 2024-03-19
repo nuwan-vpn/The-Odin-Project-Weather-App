dfefe91a64a300c9169d95b74d093d4b7941d0c6
Nuwan Vidanapathiranage <nuwanpvp@gmail.com>
2024-03-19 22:33:45 +0530

    Fix getElementById calls in fetchCurrentWeather and clearSearch functions

--- src/appInitializer.js
+++ src/appInitializer.js
@@ -8,8 +8,8 @@ export function initializeApp() {
 
 async function fetchCurrentWeather(searchCity, searchState, searchCountry) {
     try {
-        const searchCity = document.getElementById("#city-input").value;
-        const searchCountry = document.getElementById("#country-input").value;
+        const searchCity = document.getElementById("city-input").value;
+        const searchCountry = document.getElementById("country-input").value;
         
         // Run check to ensure City and Country fields have values
         if (searchCity == "" || searchCountry == "") {
@@ -21,7 +21,8 @@ async function fetchCurrentWeather(searchCity, searchState, searchCountry) {
         console.log(searchCountry);
 
           // Run fetch and wait for response JSON
-          const response = await fetch("https://api.weatherapi.com/v1/current.json?q=" + searchCity + "," + searchCountry + "&units=imperial&APPID=d1f87e6c9c7218274f8b83bf9e900783", { mode: "cors"});
+          const response = await fetch("https://api.weatherapi.com/v1/current.json?key=8b3539c27d4545c3a1b153426241903&q=" 
+                            + searchCity + "," + searchCountry , { mode: "cors"});
           const currentData = await response.json();
           console.log("Fetching current weather data from API....", currentData);
           
@@ -70,7 +71,7 @@ function displayWeather(currentWeather) {
 }
 
 function clearSearch() {
-    document.getElementById("#city-input").value = "";
-    document.getElementById("#country-input").value = "";
+    document.getElementById("city-input").value = "";
+    document.getElementById("country-input").value = "";
 }
 
